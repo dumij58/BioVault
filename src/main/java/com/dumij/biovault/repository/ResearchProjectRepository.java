6f4cd66ef38fab43fab27c80c9f835fb3f21639b
kulani <shkulani03@gmail.com>
2026-07-31 21:16:45 +0530

    Add ResearchProject repository

--- src/main/java/com/dumij/biovault/repository/ResearchProjectRepository.java
+++ src/main/java/com/dumij/biovault/repository/ResearchProjectRepository.java
@@ -1,4 +1,11 @@
 package com.dumij.biovault.repository;
 
-public class ResearchProjectRepository {
+import com.dumij.biovault.model.ResearchProject;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ResearchProjectRepository extends MongoRepository<ResearchProject, String> {
+
 }
