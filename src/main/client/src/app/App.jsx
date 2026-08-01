cb78fac138c1c37c1d1c3decc5987b0b822181bc
kulani <shkulani03@gmail.com>
2026-08-01 12:14:57 +0530

    fix the navigation issue

--- src/main/client/src/app/App.jsx
+++ src/main/client/src/app/App.jsx
@@ -24,6 +24,7 @@ export function App() {
           <Home
             onLoginClick={() => navigateTo('auth')}
             onRegisterClick={() => navigateTo('registration')}
+            onProjectsClick={() => navigateTo('projects')}
           />
         )}
         {page === 'auth' && (
